7f2d23c0b52ec9a7856e635707d6dd60a44c0e7d
praveenkumarpke <praveenpke@gmail.com>
2022-06-08 16:29:04 +0530

    favicon added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Calc from './component/page/Calc';
-
+import GitHubIcon from '@mui/icons-material/GitHub';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
+      <a className='d-flex justify-content-center ml mt-2' href="https://github.com/praveenkumarpke/Basic_Calculator" target="_blank"> <GitHubIcon /> </a>
       <Calc />
     </div>
   );
